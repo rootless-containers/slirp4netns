rootless-containers/slirp4netns

commit 76d4ceff9ecfd0f0dfc25afe7389a00c65fe9e12

slirp: add a new option -e FD
if specified it tells slirp4netns what FD must be monitored for
events.  On an event on the FD the program exits with success.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

Makefile.am
@@ -5,6 +5,7 @@ AM_CFLAGS = -I $(abs_top_srcdir)/rd235_libslirp/include -I$(abs_top_srcdir)/rd23
 noinst_LIBRARIES = libqemu_slirp.a libslirp.a
 
 EXTRA_DIST = \
+	slirp.h \
 	qemu/include/qemu/queue.h \
 	qemu/slirp/slirp.h \
 	qemu/slirp/tcp_var.h \

main.c
@@ -15,6 +15,7 @@
 #include <arpa/inet.h>
 #include <net/route.h>
 #include <stdbool.h>
+#include "slirp.h"
 
 static int nsenter(pid_t target_pid)
 {
@@ -203,9 +204,7 @@ static int recvfd(int sock)
 	return fd;
 }
 
-int do_slirp(int tapfd);
-
-static int parent(int sock)
+static int parent(int sock, int exit_fd)
 {
 	int rc, tapfd;
 	if ((tapfd = recvfd(sock)) < 0) {
@@ -213,7 +212,7 @@ static int parent(int sock)
 	}
 	fprintf(stderr, "received tapfd=%d\n", tapfd);
 	close(sock);
-	if ((rc = do_slirp(tapfd)) < 0) {
+	if ((rc = do_slirp(tapfd, exit_fd)) < 0) {
 		fprintf(stderr, "do_slirp failed\n");
 		close(tapfd);
 		return rc;
@@ -224,19 +223,29 @@ static int parent(int sock)
 
 static void usage(const char *argv0)
 {
-	fprintf(stderr, "Usage: %s [-c] PID TAPNAME\n", argv0);
+	fprintf(stderr, "Usage: %s [-c] [-e FD] PID TAPNAME\n", argv0);
 }
 
 // caller needs to free tapname
-static void parse_args(int argc, char *const argv[], pid_t *ptarget_pid, char **tapname, bool * do_config_network)
+static void parse_args(int argc, char *const argv[], pid_t *ptarget_pid, char **tapname, bool * do_config_network,
+		       int *exit_fd)
 {
 	int opt;
 	int target_pid;
 
-	while ((opt = getopt(argc, argv, "c")) != -1) {
+	while ((opt = getopt(argc, argv, "ce:")) != -1) {
 		switch (opt) {
 		case 'c':
 			*do_config_network = true;
+			break;
+		case 'e':
+			errno = 0;
+			*exit_fd = strtol(optarg, NULL, 10);
+			if (errno) {
+				perror("strtol");
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+			}
 			break;
 		default:
 			usage(argv[0]);
@@ -263,9 +272,10 @@ int main(int argc, char *const argv[])
 	int sv[2];
 	pid_t target_pid, child_pid;
 	char *tapname;
+	int exit_fd = -1;
 	bool do_config_network = false;
 
-	parse_args(argc, argv, &target_pid, &tapname, &do_config_network);
+	parse_args(argc, argv, &target_pid, &tapname, &do_config_network, &exit_fd);
 	if (socketpair(AF_LOCAL, SOCK_STREAM, 0, sv) < 0) {
 		perror("socketpair");
 		exit(EXIT_FAILURE);
@@ -293,7 +303,7 @@ int main(int argc, char *const argv[])
 			fprintf(stderr, "child failed(%d)\n", child_status);
 			exit(child_status);
 		}
-		if (parent(sv[0]) < 0) {
+		if (parent(sv[0], exit_fd) < 0) {
 			fprintf(stderr, "parent failed\n");
 			exit(EXIT_FAILURE);
 		}

slirp.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
-#include "slirp.h"
+#include "qemu/slirp/slirp.h"
 #include "libslirp.h"
 
 struct libslirp_data {
@@ -43,13 +43,17 @@ Slirp *create_slirp(void *opaque)
 
 #define ETH_BUF_SIZE (65536)
 
-int do_slirp(int tapfd)
+int do_slirp(int tapfd, int exitfd)
 {
+	int ret = -1;
 	Slirp *slirp = NULL;
 	uint8_t *buf = NULL;
 	struct libslirp_data opaque = {.tapfd = tapfd };
-	GArray pollfds;
+	GArray pollfds = { 0 };
+	size_t n_fds = 1;
 	struct pollfd tap_pollfd = { tapfd, POLLIN | POLLHUP, 0 };
+	struct pollfd exit_pollfd = { exitfd, POLLHUP, 0 };
+
 	slirp = create_slirp((void *)&opaque);
 	if (slirp == NULL) {
 		fprintf(stderr, "create_slirp failed\n");
@@ -60,10 +64,14 @@ int do_slirp(int tapfd)
 		goto err;
 	}
 	g_array_append_val(&pollfds, tap_pollfd);
+	if (exitfd >= 0) {
+		n_fds++;
+		g_array_append_val(&pollfds, exit_pollfd);
+	}
 	while (1) {
 		int pollout;
 		uint32_t timeout = -1;
-		pollfds.len = 1;
+		pollfds.len = n_fds;
 		slirp_pollfds_fill(&pollfds, &timeout);
 		update_ra_timeout(&timeout);
 		do
@@ -72,6 +80,7 @@ int do_slirp(int tapfd)
 		if (pollout < 0) {
 			goto err;
 		}
+
 		if (pollfds.pfd[0].revents) {
 			ssize_t rc = read(tapfd, buf, ETH_BUF_SIZE);
 			if (rc < 0) {
@@ -80,15 +89,24 @@ int do_slirp(int tapfd)
 			}
 			slirp_input(slirp, buf, (int)rc);
  after_slirp_input:
-			pollout--;
+			pollout = -1;
 		}
+
+		/* The exitfd is closed.  */
+		if (exitfd >= 0 && pollfds.pfd[1].revents) {
+			fprintf(stderr, "exitfd event\n");
+			goto success;
+		}
+
 		slirp_pollfds_poll(&pollfds, (pollout <= 0));
 		check_ra_timeout();
 	}
+ success:
+	ret = 0;
  err:
 	fprintf(stderr, "do_slirp is exiting\n");
 	if (buf != NULL) {
 		free(buf);
 	}
-	return -1;
+	return ret;
 }

slirp.h
@@ -1,0 +1,6 @@
+#ifndef SLIRP_H
+# define SLIRP_H
+
+int do_slirp(int tapfd, int exitfd);
+
+#endif

commit 241593bdaa6a1b15dab3e80abeb004a08c6ea140

rename slirp.h to slirp4netns.h
it avoids a clash with the same file from qemu.

Bug introduced with 76d4ceff9ecfd0f0dfc25afe7389a00c65fe9e12

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

Makefile.am
@@ -5,7 +5,7 @@ AM_CFLAGS = -I $(abs_top_srcdir)/rd235_libslirp/include -I$(abs_top_srcdir)/rd23
 noinst_LIBRARIES = libqemu_slirp.a libslirp.a
 
 EXTRA_DIST = \
-	slirp.h \
+	slirp4netns.h \
 	qemu/include/qemu/queue.h \
 	qemu/slirp/slirp.h \
 	qemu/slirp/tcp_var.h \
@@ -59,5 +59,5 @@ libqemu_slirp_a_SOURCES = \
 
 libslirp_a_SOURCES = rd235_libslirp/src/qemu2libslirp.c
 
-slirp4netns_SOURCES = main.c slirp.c
+slirp4netns_SOURCES = main.c slirp4netns.c
 slirp4netns_LDADD = libslirp.a libqemu_slirp.a

main.c
@@ -15,7 +15,7 @@
 #include <arpa/inet.h>
 #include <net/route.h>
 #include <stdbool.h>
-#include "slirp.h"
+#include "slirp4netns.h"
 
 static int nsenter(pid_t target_pid)
 {