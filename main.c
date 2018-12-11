7e27c0c64d39445cc9ea692f9a3d07ef427dab8d
Anton Semjonov <anton@semjonov.de>
2018-12-11 16:39:23 +0100

    use macros in configure.ac to define VERSION and COMMIT in config.h
    Signed-off-by: Anton Semjonov <anton@semjonov.de>

--- main.c
+++ main.c
@@ -250,10 +250,7 @@ static void usage(const char *argv0)
 // version output is runc-compatible and machine-parsable
 static void version()
 {
-#ifndef GITVERSION
-#define GITVERSION VERSION
-#endif
-	printf("slirp4netns version %s\n", GITVERSION ? GITVERSION : PACKAGE_VERSION);
+	printf("slirp4netns version %s\n", VERSION ? VERSION : PACKAGE_VERSION);
 #ifdef COMMIT
 	printf("commit: %s\n", COMMIT);
 #endif
