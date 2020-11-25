c441641117f2e62344c2ea75e7300c7ebd848f85
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-11-25 17:47:14 +0900

    tests/common.sh: fix nsenter flags
    `nsenter` should not be called when `unshare` was called without `-r`.
    
    Close #235
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- tests/common.sh
+++ tests/common.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
+function nsenter_flags {
+  pid=$1
+  flags="--target=${pid}"
+  userns="$(readlink /proc/${pid}/ns/user)"
+  mntns="$(readlink /proc/${pid}/ns/mnt)"
+  netns="$(readlink /proc/${pid}/ns/net)"
+
+  self_userns="$(readlink /proc/self/ns/user)"
+  self_mntns="$(readlink /proc/self/ns/mnt)"
+  self_netns="$(readlink /proc/self/ns/net)"
+
+  if [ "${userns}" != "${self_userns}" ]; then
+    flags="$flags --preserve-credentials -U"
+  fi
+  if [ "${mntns}" != "${self_mntns}" ]; then
+    flags="$flags -m"
+  fi
+  if [ "${netns}" != "${self_netns}" ]; then
+    flags="$flags -n"
+  fi
+  echo "${flags}"
+}
+
 function wait_for_network_namespace {
     # Wait that the namespace is ready.
     COUNTER=0
     while [ $COUNTER -lt 40 ]; do
-        if nsenter --preserve-credentials -U -n --target=$1 true; then
+        flags=$(nsenter_flags $1)
+        if $(echo $flags | grep -qvw -- -n); then
+          flags="$flags -n"
+        fi
+        if nsenter ${flags} true >/dev/null 2>&1; then
             break
         else
             sleep 0.5
@@ -17,7 +44,7 @@ function wait_for_network_device {
     # Wait that the device appears.
     COUNTER=0
     while [ $COUNTER -lt 40 ]; do
-        if nsenter --preserve-credentials -U -n --target=$1 ip addr show $2; then
+        if nsenter $(nsenter_flags $1) ip addr show $2; then
             break
         else
             sleep 0.5
@@ -41,7 +68,7 @@ function wait_process_exits {
 function wait_for_ping_connectivity {
     COUNTER=0
     while [ $COUNTER -lt 40 ]; do
-        if nsenter --preserve-credentials -U -n --target=$1 ping -c 1 -w 1 $2; then
+        if nsenter $(nsenter_flags $1) ping -c 1 -w 1 $2; then
             break
         else
             sleep 0.5
@@ -53,7 +80,7 @@ function wait_for_ping_connectivity {
 function wait_for_connectivity {
     COUNTER=0
     while [ $COUNTER -lt 40 ]; do
-        if echo "wait_for_connectivity" | nsenter --preserve-credentials -U -n --target=$1 ncat -v $2 $3; then
+        if echo "wait_for_connectivity" | nsenter $(nsenter_flags $1) ncat -v $2 $3; then
             break
         else
             sleep 0.5
