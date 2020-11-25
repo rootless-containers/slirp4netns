77bd1c7408eed2184be1f851e90d41d76f42a87e
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-11-25 16:36:57 +0900

    seccomp: block more syscalls
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- seccompfilter_rules.h
+++ seccompfilter_rules.h
@@ -4,18 +4,59 @@
 #ifndef BLOCK
 #error "Included in an unexpected way?"
 #endif
-BLOCK(execve);
-/* ideally we should also block open() and openat() but required for resolv.conf
+/*
+  NOTE:
+  - Run `sudo systemd-analyze syscall-filter` to show list of syscall groups.
+  - Ideally we should also block open() and openat(), but these calls are required for opening resolv.conf
  */
-BLOCK(open_by_handle_at);
+
+/* group: @default */
+BLOCK(execve);
+
+/* group: @debug */
+BLOCK(lookup_dcookie);
+BLOCK(pidfd_getfd);
 BLOCK(ptrace);
-BLOCK(prctl);
+
+/* group: @ipc */
 BLOCK(process_vm_readv);
 BLOCK(process_vm_writev);
+
+/* group: @module*/
+BLOCK(delete_module);
+BLOCK(finit_module);
+BLOCK(init_module);
+
+/* group: @mount */
+BLOCK(chroot);
+BLOCK(fsconfig);
+BLOCK(fsmount);
+BLOCK(fsopen);
+BLOCK(fspick);
 BLOCK(mount);
-BLOCK(name_to_handle_at);
-BLOCK(setns);
+BLOCK(move_mount);
+BLOCK(open_tree);
+BLOCK(pivot_root);
 BLOCK(umount);
 BLOCK(umount2);
+
+/* group: @privileged */
+BLOCK(open_by_handle_at);
+
+/* group: @process */
+BLOCK(execveat);
+BLOCK(pidfd_open);
+BLOCK(pidfd_send_signal);
+BLOCK(prctl);
+BLOCK(setns);
 BLOCK(unshare);
+
+/* group: @reboot */
+BLOCK(kexec_file_load);
+BLOCK(kexec_load);
+BLOCK(reboot);
+
+/* group: @system-service */
+BLOCK(name_to_handle_at);
+
 #endif
